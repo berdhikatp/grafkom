25e089f131e01381d564b54931240adcdf28a215
bedha <berdhika@gmail.com>
2020-04-19 14:28:52 +0700

    wes tak rotate pisan bre

--- gelas.js
+++ gelas.js
@@ -1,73 +1,52 @@
 var myCanvas = document.getElementById('canvas');
 var context = myCanvas.getContext('2d');
+myCanvas.width= innerWidth;
+myCanvas.height = innerHeight
 
-myCanvas.width = myCanvas.scrollWidth;
-myCanvas.height = myCanvas.scrollHeight;
-myCanvas.width = myCanvas.scrollWidth;
-myCanvas.height = myCanvas.scrollHeight;
-
-
-context.beginPath();
-context.moveTo(80, 160);
-context.lineTo(80, 130);
-context.lineTo(250, 130);
-context.lineTo(250, 160);
-context.closePath();
-context.lineWidth = 2;
-context.strokeStyle='black';
-context.fillStyle ='#0095dd';
-context.fill();
-context.stroke();
-
-context.beginPath();
-context.moveTo(80, 160);
-context.lineTo(250, 160);
-context.lineTo(250, 390);
-context.lineTo(80, 390);
-context.closePath();
-context.lineWidth = 2;
-context.strokeStyle='black';
-context.fillStyle = '#0095dd';
-context.fill();
-context.stroke();
+//gelas 1
+//pegangan tutup gelas
+context.fillStyle = "blue";
+context.fillRect(165,180,30,50);
+//tutup gelas
+context.fillStyle = "lightblue";
+context.fillRect(100,200,160,50);
+//badan gelas
+context.fillStyle = "blue";
+context.fillRect(100,240,160,200);
 
 
-context.beginPath();
-context.moveTo(150, 130);
-context.lineTo(150, 100);
-context.lineTo(175, 100);
-context.lineTo(175, 130);
-context.closePath();
-context.lineWidth = 2;
-context.strokeStyle='black';
-context.fillStyle ='#0095dd';
-context.fill();
-context.stroke();
+
+
+//gelas 2
+//pegangan tutup gelas
+context.strokeStyle = "blue";
+context.strokeRect(365,180,30,20);
+//tutup gelas
+context.strokeStyle = "black";
+context.strokeRect(300,200,160,41);
+//badan gelas
+context.fillStyle = "blue";
+context.fillRect(300,240,160,200);
+
 
 
 //gelas 3
+//pegangan tutup gelas
+context.fillStyle = "blue";
+context.fillRect(565,180,30,50);
+//tutup gelas
+context.fillStyle = "lightblue";
+context.fillRect(500,200,160,50);
+//badan gelas
+context.fillStyle = "blue";
+context.fillRect(500,240,160,200);
 
-context.beginPath();
-context.rotate ((Math.PI/180)*25);
-context.moveTo(80, 160);
-context.lineTo(80, 130);
-context.lineTo(250, 130);
-context.lineTo(250, 160);
-context.closePath();
-context.lineWidth = 2;
-context.strokeStyle='black';
-context.fillStyle ='#0095dd';
-context.fill();
-context.stroke();
 
-context.beginPath();
-context.moveTo(80, 160);
-context.lineTo(250, 160);
-context.lineTo(250, 390);
-context.lineTo(80, 390);
-context.closePath();
-context.lineWidth = 2;
-context.strokeStyle='black';
-context.fillStyle = '#0095dd';
-context.fill();
-context.stroke();
+//rotasi tutup gelas
+context.rotate ((Math.PI/180)*40);
+//tutup gelas
+context.fillStyle = "lightblue";
+context.fillRect(335,-149,160,41);
+//peganga
+context.fillStyle = "blue";
+context.fillRect(395,-178,25,30);
