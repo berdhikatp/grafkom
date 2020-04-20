b5e71519f2fe0bd501f5bb3bf2f04204e998accb
martafairus1203 <martafairus123@gmail.com>
2020-04-21 00:13:41 +0700

    gelas 1 2 dah beres posisi

--- gelas.js
+++ gelas.js
@@ -6,31 +6,31 @@ myCanvas.height = innerHeight
 //gelas 1 berdha
 //pegangan tutup gelas
 context.fillStyle = "blue";
-context.fillRect(165,180,30,50);
+context.fillRect(285,320,30,50);
 //tutup gelas
 context.fillStyle = "lightblue";
-context.fillRect(100,200,160,50);
+context.fillRect(220,340,160,50);
 //badan gelas
 context.fillStyle = "blue";
-context.fillRect(100,240,160,200);
+context.fillRect(220,380,160,200);
 
 
 //gelas 2 martafairuss
 //pegangan tutup gelas
 context.strokeStyle = "blue";
-context.strokeRect(365,180,30,20);
+context.strokeRect(695,320,30,20);
 //tutup gelas
 context.strokeStyle = "black";
-context.strokeRect(300,200,160,41);
+context.strokeRect(630,340,160,41);
 //badan gelas
 context.fillStyle = "blue";
-context.fillRect(300,240,160,200);
+context.fillRect(630,382,160,200);
 
 //rotasi tutup gelas
 context.rotate ((Math.PI/180)*40);
 //tutup gelas
 context.fillStyle = "lightblue";
-context.fillRect(335,-149,160,41);
+context.fillRect(689,-257,160,41);
 //pegangan
 context.fillStyle = "blue";
-context.fillRect(395,-178,25,30);
+context.fillRect(755,-278,30,22);
