e01ca574ad5fc634b42f5ace3955ce2f6fe7b5c1
LindaFatmawatiAgustina <lindawatiagustina132@gmail.com>
2020-04-21 01:05:35 +0700

    alhamdulillah dah bisa

--- gelas.js
+++ gelas.js
@@ -35,7 +35,7 @@ context.fillRect(689,-257,160,41);
 context.fillStyle = "blue";
 context.fillRect(755,-278,30,22);
 
-//gelas 3 linda ready
+//gelas 3 linda readyyyyy
 //pegangan tutup gelas
 context.fillStyle = "blue";
 context.fillRect(565,180,30,50);
