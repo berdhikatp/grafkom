391a3a83f50e8b1d6fbee076d3e90adb8f5146b0
martafairus1203 <martafairus123@gmail.com>
2020-04-20 19:28:01 +0700

    gelas dua beres, lanjott

--- gelas.js
+++ gelas.js
@@ -13,3 +13,15 @@ context.fillRect(100,200,160,50);
 //badan gelas
 context.fillStyle = "blue";
 context.fillRect(100,240,160,200);
+
+
+//gelas 2 martafairuss
+//pegangan tutup gelas
+context.strokeStyle = "blue";
+context.strokeRect(365,180,30,20);
+//tutup gelas
+context.strokeStyle = "black";
+context.strokeRect(300,200,160,41);
+//badan gelas
+context.fillStyle = "blue";
+context.fillRect(300,240,160,200);
