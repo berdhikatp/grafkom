ab769529b1935f64b2e6c1f3ca8faaeee409d2d1
arumindahsari <arumindahsari03@gmail.com>
2020-04-21 09:28:05 +0700

    ubah dikit

--- gelas.js
+++ gelas.js
@@ -1,50 +1,79 @@
 var myCanvas = document.getElementById('canvas');
 var context = myCanvas.getContext('2d');
-myCanvas.width= innerWidth;
-myCanvas.height = innerHeight
+myCanvas.width = innerWidth;
+myCanvas.height = innerHeight;
+
+
+//teks arum sipp
+context.font = 'bold 15pt comic';
+context.lineWidth = 3;
+context.fillStyle = 'black';
+context.textBaseline = 'middle';
+context.fillText('Gelas Tertutup', 235, 620);
+context.font = 'bold 30pt comic';
+context.fillStyle = 'black';
+context.fillText('Grafika Komputer', 550, 80);
+context.font = 'bold 20pt comic';
+context.fillStyle = 'black';
+context.fillText('Membuat Gelas Terbuka dan Tertutup', 480, 120);
+context.font = 'bold 15pt comic';
+context.fillStyle = 'black';
+context.fillText('Gelas 1/2 Terbuka', 635, 620);
+context.font = 'bold 15pt comic';
+context.fillStyle = 'black';
+context.fillText('Gelas Tertutup', 1055, 620);
+
 
 //gelas 1 berdha
 //pegangan tutup gelas
-context.fillStyle = "blue";
-context.fillRect(285,320,30,50);
+context.fillStyle = "salmon";
+context.fillRect(285, 320, 30, 50);
 //tutup gelas
-context.fillStyle = "lightblue";
-context.fillRect(220,340,160,50);
+context.fillStyle = "#FFB6C1";
+context.fillRect(220, 340, 160, 50);
 //badan gelas
-context.fillStyle = "blue";
-context.fillRect(220,380,160,200);
+context.fillStyle = "salmon";
+context.fillRect(220, 380, 160, 200);
 
 
 //gelas 2 martafairuss
-//badan gelas
-context.fillStyle = "blue";
-context.fillRect(630,382,160,200);
 //pegangan tutup gelas
-context.strokeStyle = "blue";
-context.strokeRect(695,320,30,20);
+context.strokeStyle = "salmon";
+context.strokeRect(695, 320, 30, 20);
 //tutup gelas
 context.strokeStyle = "black";
-context.strokeRect(630,340,160,41);
-//rotasi tutup gelas
-context.rotate ((Math.PI/180)*40);
-//tutup gelas
-context.fillStyle = "lightblue";
-context.fillRect(689,-257,160,41);
-//pegangan
-context.fillStyle = "blue";
-context.fillRect(755,-278,30,22);
-
-
+context.strokeRect(630, 340, 160, 41);
+//badan gelas
+context.fillStyle = "salmon";
+context.fillRect(630, 382, 160, 200);
 
 //gelas 3 linda readyy
 //pegangan tutup gelas
-context.fillStyle = "blue";
-context.fillRect(565,180,30,50);
+context.fillStyle = "salmon";
+context.fillRect(1100, 320, 30, 50);
 //tutup gelas
-context.fillStyle = "lightblue";
-context.fillRect(500,200,160,50);
+context.fillStyle = "#FFB6C1";
+context.fillRect(1040, 340, 160, 50);
 //badan gelas
-context.fillStyle = "blue";
-context.fillRect(500,240,160,200);
+context.fillStyle = "salmon";
+context.fillRect(1040, 390, 160, 200);
+
+//rotasi tutup gelas
+context.rotate((Math.PI / 180) * 40);
+//tutup gelas
+context.fillStyle = "#FFB6C1";
+context.fillRect(689, -257, 160, 41);
+//pegangan
+context.fillStyle = "salmon";
+context.fillRect(755, -278, 30, 22);
+
+//lanjutan text
+
+var x = myCanvas.width / 2;
+var y = myCanvas.height / 2;
+
+
+
+
 
 
